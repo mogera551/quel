c47e1adbc07783a0ce91dfe391c77dc7580e60f2
mogera551 <mogera551@gmail.com>
2023-02-18 15:42:51 +0900

    [add]タグ名プリフィックス

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import Component from "./component/Component.js";
 import "./thread/Thread.js"; // threadの開始
 
 export default class {
+  static #prefix;
   /**
    * 
    * @param {Object<string,UserComponentData>} components 
    */
   static components(components) {
-    Object.entries(components).forEach(([name,componentData]) => {
-      Component.regist(name, componentData);
+    const prefix = this.#prefix;
+    Object.entries(components).forEach(([name, componentData]) => {
+      const componentName = prefix ? `${prefix}-${name}` : name;
+      Component.regist(componentName, componentData);
     });
-
+    return this;
+  }
+  static prefix(prefix) {
+    this.#prefix = prefix;
+    return this;
   }
 }
