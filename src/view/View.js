c83ea65483f5a441e2763dbb45124d577be94e0e
mogera551 <mogera551@gmail.com>
2023-10-16 07:56:14 +0900

    [refactor]bind

--- src/view/View.js
+++ src/view/View.js
@@ -9,13 +9,13 @@ export class ViewTemplate {
    * @param {Component} component 
    * @param {HTMLTemplateElement} template
    * @param {ContextInfo} context
-   * @returns {{binds:BindInfo[], content:DocumentFragment}}
+   * @returns {{bindings:import("../binding/Binding.js").Binding[], content:DocumentFragment}}
    */
   static render(component, template, context) {
     const content = document.importNode(template.content, true); // See http://var.blog.jp/archives/76177033.html
     const nodes = Selector.getTargetNodes(template, content);
-    const binds = Binder.bind(nodes, component, context);
-    return { binds, content };
+    const bindings = Binder.bind(nodes, component, context);
+    return { bindings, content };
   }
 }
 
@@ -25,11 +25,11 @@ export class View {
    * @param {HTMLElement} rootElement 
    * @param {Component} component 
    * @param {HTMLTemplateElement} template 
-   * @returns {BindInfo[]}
+   * @returns {import("../binding/Binding.js").Binding[]}
    */
   static render(rootElement, component, template) {
-    const { binds, content } = ViewTemplate.render(component, template, Context.create());
+    const { bindings, content } = ViewTemplate.render(component, template, Context.create());
     rootElement.appendChild(content);
-    return binds;
+    return bindings;
   }
 }
