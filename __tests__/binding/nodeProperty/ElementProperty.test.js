57a8542a80f09d6a57a7ed4f4ade59c7dec138e3
mogera551 <mogera551@gmail.com>
2023-10-26 16:25:49 +0900

    [refactor]bind

--- __tests__/binding/nodeProperty/ElementProperty.test.js
+++ __tests__/binding/nodeProperty/ElementProperty.test.js
@@ -1,15 +1,22 @@
 import { ElementProperty } from "../../../src/binding/nodePoperty/ElementProperty.js";
 
+const binding = {};
+
 test("ElementProperty", () => {
   const element = document.createElement("div");
   element.textContent = "abc";
-  const elementProperty = new ElementProperty(element, "textContent", [], {});
+  const elementProperty = new ElementProperty(binding, element, "textContent", [], {});
+  expect(elementProperty.binding).toBe(binding);
   expect(elementProperty.node).toBe(element);
   expect(elementProperty.element).toBe(element);
+  expect(elementProperty.name).toBe("textContent");
+  expect(elementProperty.nameElements).toEqual(["textContent"]);
+  expect(elementProperty.filters).toEqual([]);
+  expect(elementProperty.filterFuncs).toEqual({});
   expect(elementProperty.applicable).toBe(true);
 
   expect(() => {
     const node = document.createTextNode("abc");
-    const elementProperty = new ElementProperty(node, "textContent", [], {});
+    const elementProperty = new ElementProperty(binding, node, "textContent", [], {});
   }).toThrow("not element");
 })
