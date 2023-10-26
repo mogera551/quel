ccd5aef8d5b54616306000cf7cac2506ea3ec84d
mogera551 <mogera551@gmail.com>
2023-10-26 15:19:03 +0900

    [refactor]bind

--- __tests__/binding/nodeProperty/ElementClass.test.js
+++ __tests__/binding/nodeProperty/ElementClass.test.js
@@ -1,10 +1,22 @@
 import { ElementClass } from "../../../src/binding/nodePoperty/ElementClass.js";
 
+const binding = {};
+
 test("ElementClass", () => {
   const element = document.createElement("div");
-  const elementClass = new ElementClass(element, "class.selected", [], {});
+  const elementClass = new ElementClass(binding, element, "class.selected", [], {});
+  expect(elementClass.binding).toBe(binding);
+  expect(elementClass.node).toBe(element);
+  expect(elementClass.element).toBe(element);
+  expect(elementClass.name).toBe("class.selected");
+  expect(elementClass.nameElements).toEqual(["class", "selected"]);
   expect(elementClass.className).toBe("selected");
+  expect(elementClass.filters).toEqual([]);
+  expect(elementClass.filterFuncs).toEqual({});
   expect(elementClass.value).toBe(false);
+  expect(elementClass.filteredValue).toBe(false);
+  expect(elementClass.applicable).toBe(true);
+
   expect(elementClass.applicable).toBe(true);
 
   element.className = "selected";
