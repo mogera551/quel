e9e57abe9ad1491fdebc393eb5ad7c4d8761b484
mogera551 <mogera551@gmail.com>
2023-10-26 15:14:51 +0900

    [refactor]bind

--- __tests__/binding/nodeProperty/ElementAttribute.test.js
+++ __tests__/binding/nodeProperty/ElementAttribute.test.js
@@ -1,15 +1,25 @@
 import { ElementAttribute } from "../../../src/binding/nodePoperty/ElementAttribute.js";
 
+const binding = {};
+
 test("ElementAttribute", () => {
   const element = document.createElement("div");
   element.setAttribute("title", "abc");
-  const elementAttribute = new ElementAttribute(element, "attr.title", [], {});
+  const elementAttribute = new ElementAttribute(binding, element, "attr.title", [], {});
+  expect(elementAttribute.binding).toBe(binding);
+  expect(elementAttribute.node).toBe(element);
+  expect(elementAttribute.element).toBe(element);
+  expect(elementAttribute.name).toBe("attr.title");
+  expect(elementAttribute.nameElements).toEqual(["attr", "title"]);
   expect(elementAttribute.attributeName).toBe("title");
+  expect(elementAttribute.filters).toEqual([]);
+  expect(elementAttribute.filterFuncs).toEqual({});
   expect(elementAttribute.value).toBe("abc");
+  expect(elementAttribute.filteredValue).toBe("abc");
   expect(elementAttribute.applicable).toBe(true);
-  expect(element.title).toBe("abc");
 
+  expect(element.getAttribute("title")).toBe("abc");
   elementAttribute.value = "def";
   expect(elementAttribute.value).toBe("def");
-  expect(element.title).toBe("def");
+  expect(element.getAttribute("title")).toBe("def");
 });
