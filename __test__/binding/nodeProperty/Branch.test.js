b1a05a44e530e4bdef828367a09c125893ce03d2
mogera551 <mogera551@gmail.com>
2024-02-08 07:52:21 +0900

    refactor:unit test

--- __test__/binding/nodeProperty/Branch.test.js
+++ __test__/binding/nodeProperty/Branch.test.js
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { BindingManager } from "../../../src/binding/Binding.js";
 import { Branch } from "../../../src/binding/nodeProperty/Branch.js";
 import { utils } from "../../../src/utils.js";
@@ -7,6 +8,7 @@ describe("Branch", () => {
   let branch;
   let uuid = utils.createUUID();
   let template1, template2;
+  let binding;
 
   beforeEach(() => {
     Templates.templateByUUID.clear();
@@ -18,7 +20,8 @@ describe("Branch", () => {
     const name = "if"; // Mock property name
     const filters = []; // Mock filters array
     const inputFilterFuncs = {}; // Mock input filter functions object
-    const binding = {
+    binding = {
+      component: {},
       children: [],
       appendChild: () => {},
     }; // Mock binding object
@@ -29,13 +32,94 @@ describe("Branch", () => {
     expect(branch.value).toBe(false);
   });
 
-  it("should set the value correctly", () => {
-    const binding_appendChild = jest.spyOn(binding, "appendChild").mockImplementation((bindingManager) => {});
-    const newValue = true;
-    branch.value = newValue;
-    expect(branch.value).toBe(newValue);
+  it("should set to value property", () => {
+    const retBindingManager = {
+      dispose: () => {},
+      applyToNode: () => {},
+    }
+    const bindingManager_create = jest.spyOn(BindingManager, "create").mockImplementation((component, template, binding) => {
+      return retBindingManager;
+    });
+    const bindingManager_dispose = jest.spyOn(retBindingManager, "dispose").mockImplementation(() => {
+    });
+    const bindingManager_applyToNode = jest.spyOn(retBindingManager, "applyToNode").mockImplementation(() => {
+    });
+    const binding_appendChild = jest.spyOn(binding, "appendChild").mockImplementation((bindingManager) => {
+      binding.children.push(bindingManager);
+    });
+
+    bindingManager_create.mockClear();
+    bindingManager_dispose.mockClear();
+    bindingManager_applyToNode.mockClear();
+    binding_appendChild.mockClear();
+    const trueValue = true;
+    branch.value = trueValue;
+    expect(branch.value).toBe(trueValue);
+    expect(bindingManager_create.mock.calls.length).toBe(1);
+    expect(bindingManager_create.mock.calls[0]).toEqual([binding.component, template1, binding]);
     expect(binding_appendChild.mock.calls.length).toBe(1);
-    //expect(binding_appendChild.mock.calls[0]).toEqual([elementEvent.eventType, elementEvent.handler]);
+    expect(binding_appendChild.mock.calls[0]).toEqual([retBindingManager]);
+    expect(bindingManager_applyToNode.mock.calls.length).toBe(0);
+    expect(bindingManager_dispose.mock.calls.length).toBe(0);
+    expect(binding.children.length).toBe(1);
+    expect(binding.children[0]).toBe(retBindingManager);
+
+    bindingManager_create.mockClear();
+    bindingManager_dispose.mockClear();
+    bindingManager_applyToNode.mockClear();
+    binding_appendChild.mockClear();
+    const falseValue = false;
+    branch.value = falseValue;
+    expect(branch.value).toBe(falseValue);
+    expect(bindingManager_dispose.mock.calls.length).toBe(1);
+    expect(bindingManager_dispose.mock.calls[0]).toEqual([]);
+    expect(bindingManager_applyToNode.mock.calls.length).toBe(0);
+    expect(bindingManager_create.mock.calls.length).toBe(0);
+    expect(binding_appendChild.mock.calls.length).toBe(0);
+    expect(binding.children.length).toBe(0);
+
+    bindingManager_create.mockClear();
+    bindingManager_dispose.mockClear();
+    bindingManager_applyToNode.mockClear();
+    binding_appendChild.mockClear();
+    branch.value = falseValue;
+    expect(branch.value).toBe(falseValue);
+    expect(bindingManager_dispose.mock.calls.length).toBe(0);
+    expect(bindingManager_create.mock.calls.length).toBe(0);
+    expect(binding_appendChild.mock.calls.length).toBe(0);
+    expect(bindingManager_applyToNode.mock.calls.length).toBe(0);
+    expect(binding.children.length).toBe(0);
+
+    branch.value = trueValue;
+    expect(branch.value).toBe(trueValue);
+
+    bindingManager_create.mockClear();
+    bindingManager_dispose.mockClear();
+    bindingManager_applyToNode.mockClear();
+    binding_appendChild.mockClear();
+    branch.value = trueValue;
+    expect(branch.value).toBe(trueValue);
+    expect(bindingManager_dispose.mock.calls.length).toBe(0);
+    expect(bindingManager_create.mock.calls.length).toBe(0);
+    expect(binding_appendChild.mock.calls.length).toBe(0);
+    expect(bindingManager_applyToNode.mock.calls.length).toBe(1);
+    expect(bindingManager_applyToNode.mock.calls[0]).toEqual([]);
+    expect(binding.children.length).toBe(1);
+    expect(binding.children[0]).toBe(retBindingManager);
+
+  });
+
+  it("should throw error for invalid property name", () => {
+    const name = "notIf"; // Mock property name
+    expect(() => {
+      new Branch(binding, document.createComment(`@@|${uuid}`), name, [], {});
+    }).toThrow(`Branch: invalid property name ${name}`);
+  });
+
+  it("should throw error for invalid value", () => {
+    expect(() => {
+      branch.value = "invalidValue";
+    }).toThrow("Branch: value is not boolean");
   });
 
   it("should same check always returned false", () => {
@@ -43,5 +127,4 @@ describe("Branch", () => {
     expect(branch.isSameValue(false)).toBe(false);
   });
 
-  // Add more test cases as needed
 });
