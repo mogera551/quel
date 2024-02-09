d096e68fb414305fe91e510b77b59c4872ae5975
mogera551 <mogera551@gmail.com>
2024-02-10 07:54:59 +0900

    refactor:unit test

--- __test__/binding/BindingSummary.test.js
+++ __test__/binding/BindingSummary.test.js
@@ -23,6 +23,7 @@ describe("BindingSummary", () => {
         constructor: NodeProperty
       }
     };
+    expect(bindingSummary.updatedBindings.size).toBe(0);
     expect(bindingSummary.allBindings.size).toBe(0);
     expect(bindingSummary.expandableBindings.size).toBe(0);
     expect(bindingSummary.componentBindings.size).toBe(0);
@@ -75,6 +76,17 @@ describe("BindingSummary", () => {
     expect(bindingSummary.bindingsByKey.get("key1")).toEqual(new Set([binding]));
     expect(bindingSummary.expandableBindings.size).toBe(1);
     expect(bindingSummary.componentBindings.size).toBe(1);
+    bindingSummary.delete(binding);
+    expect(bindingSummary.allBindings.size).toBe(1);
+    expect(bindingSummary.bindingsByKey.get("key1")).toEqual(new Set([binding]));
+    expect(bindingSummary.expandableBindings.size).toBe(1);
+    expect(bindingSummary.componentBindings.size).toBe(1);
+    bindingSummary.flush();
+    expect(bindingSummary.allBindings.size).toBe(0);
+    expect(bindingSummary.expandableBindings.size).toBe(0);
+    expect(bindingSummary.componentBindings.size).toBe(0);
+    expect(bindingSummary.bindingsByKey.get("key1")).toBe(undefined);
+
   });
 
   test("should delete a binding, flush", () => {
@@ -121,18 +133,83 @@ describe("BindingSummary", () => {
     expect(bindingSummary.allBindings.size).toBe(1);
     expect(bindingSummary.bindingsByKey.get("key1")).toEqual(new Set([binding]));
   });
-/*
+
+  test("should flush, case of delete binding >= remain bindings * 10  ", () => {
+    let lastBinding = null;
+    for(let i = 0; i < 11; i++) {
+      const binding = {
+        viewModelProperty: {
+          key: "key1"
+        },
+        nodeProperty: {
+          expandable: false,
+          constructor: NodeProperty
+        }
+      };
+      bindingSummary.add(binding);
+      lastBinding = binding;
+    }
+    const deleteBindings = Array.from(bindingSummary.allBindings).filter((_, index) => index < 10);
+    deleteBindings.forEach(binding => bindingSummary.delete(binding));
+    bindingSummary.flush();
+    expect(bindingSummary.allBindings.size).toBe(1);
+    expect(bindingSummary.bindingsByKey.get("key1")).toEqual(new Set([lastBinding]));
+  });
+
+  test("should flush, case of delete binding < remain bindings * 10  ", () => {
+    let lastBinding = null;
+    for(let i = 0; i < 12; i++) {
+      const binding = {
+        viewModelProperty: {
+          key: "key1"
+        },
+        nodeProperty: {
+          expandable: false,
+          constructor: NodeProperty
+        }
+      };
+      bindingSummary.add(binding);
+      lastBinding = binding;
+    }
+    const deleteBindings = Array.from(bindingSummary.allBindings).filter((_, index) => index < 11);
+    deleteBindings.forEach(binding => bindingSummary.delete(binding));
+    bindingSummary.flush();
+    expect(bindingSummary.allBindings.size).toBe(1);
+    expect(bindingSummary.bindingsByKey.get("key1")).toEqual(new Set([lastBinding]));
+  });
+
   test("should rebuild bindings", () => {
     const bindings = [];
+    for(let i = 0; i < 10; i++) {
+      const binding = {
+        viewModelProperty: {
+          key: "key1"
+        },
+        nodeProperty: {
+          expandable: false,
+          constructor: NodeProperty
+        }
+      };
+      bindings.push(binding);
+    }
     bindingSummary.rebuild(bindings);
     expect(bindingSummary.allBindings.size).toBe(bindings.length);
+    bindingSummary.rebuild(bindings.filter((_, index) => index < 5));
+    expect(bindingSummary.allBindings.size).toBe(5);
   });
 
   test("should clear all bindings", () => {
-    const binding = {  };
+    const binding = {
+      viewModelProperty: {
+        key: "key1"
+      },
+      nodeProperty: {
+        expandable: false,
+        constructor: NodeProperty
+      }
+    };
     bindingSummary.add(binding);
     bindingSummary.clear();
     expect(bindingSummary.allBindings.size).toBe(0);
   });
-*/
 });
