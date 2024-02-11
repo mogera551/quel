f1b05fb1fba5b2456bc04d1786656bec15899642
mogera551 <mogera551@gmail.com>
2024-02-12 07:52:34 +0900

    refactor:delete no-used method

--- src/utils.js
+++ src/utils.js
@@ -8,27 +8,6 @@ export class utils {
   }
 
   /**
-   * 関数かどうかをチェック
-   * @param {any} obj 
-   * @returns {boolean}
-   */
-  static isFunction = (obj) => {
-    const toString = Object.prototype.toString;
-    const text = toString.call(obj).slice(8, -1).toLowerCase();
-    return (text === "function" || text === "asyncfunction");
-  }
-
-  /**
-   * 
-   * @param {HTMLElement} element 
-   * @returns {boolean}
-   */
-  static isInputableElement(element) {
-    return element instanceof HTMLSelectElement || element instanceof HTMLTextAreaElement || 
-      (element instanceof HTMLInputElement && element.type !== "button");
-  }
-
-  /**
    * to kebab case (upper camel, lower camel, snakeを想定)
    * @param {string} text 
    * @returns {string}
