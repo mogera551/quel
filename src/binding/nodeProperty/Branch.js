cfc556334c128b2af0456b831ae5de9572ecd860
mogera551 <mogera551@gmail.com>
2024-07-27 05:51:24 +0900

    docs:

--- src/binding/nodeProperty/Branch.js
+++ src/binding/nodeProperty/Branch.js
@@ -1,13 +1,24 @@
+/**
+ * @module binding/nodeProperty/Branch
+ * @typedef {import("../Binding.js").Binding} Binding
+ * @typedef {import("../Binding.js").BindingManager} BindingManager
+ */
+import "../../types.js";
 import { TemplateProperty } from "./TemplateProperty.js";
 import { utils } from "../../utils.js";
 import { BindingManager } from "../Binding.js";
 
 export class Branch extends TemplateProperty {
-  /** @type {boolean} */
+  /** 
+   * @return {boolean} having child bindingManager or not
+   */
   get value() {
     return this.binding.children.length > 0;
   }
-  /** @param {boolean} value */
+  /** 
+   * Set value to bind/unbind child bindingManager
+   * @param {boolean} value 
+   */
   set value(value) {
     if (typeof value !== "boolean") utils.raise(`Branch: ${this.binding.component.selectorName}.ViewModel['${this.binding.viewModelProperty.name}'] is not boolean`, );
     if (this.value !== value) {
@@ -26,7 +37,7 @@ export class Branch extends TemplateProperty {
 
   /**
    * 
-   * @param {import("../Binding.js").Binding} binding
+   * @param {Binding} binding
    * @param {Comment} node 
    * @param {string} name 
    * @param {FilterInfo[]} filters 
