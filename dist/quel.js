1c1d0ae9ab02786d3bf73d30284c08bbc80a1f13
mogera551 <103912708+mogera551@users.noreply.github.com>
2024-09-23 08:47:12 +0900

    fix:access get

--- dist/quel.js
+++ dist/quel.js
@@ -1675,17 +1675,22 @@ let Handler$1 = class Handler {
     }
     __set(target, propInfo, indexes, value, receiver) {
         if (propInfo.paths.length === 1) {
-            return Reflect.set(target, propInfo.paths[0], value, receiver);
+            return Reflect.set(target, propInfo.name, value, receiver);
         }
         this.withIndexes(propInfo, indexes, () => {
-            const lastPatternElement = propInfo.patternElements[propInfo.patternElements.length - 1];
-            const lastElement = propInfo.elements[propInfo.elements.length - 1];
-            const parentValue = this._getValue(target, propInfo.patternPaths, propInfo.patternElements, indexes, propInfo.paths.length - 2, propInfo.wildcardCount - (lastPatternElement === "*" ? 1 : 0) - 1, receiver);
-            if (lastPatternElement === "*") {
-                parentValue[indexes[indexes.length - 1] ?? utils.raise("wildcard is undefined")] = value;
+            if (propInfo.name in target) {
+                Reflect.set(target, propInfo.name, value, receiver);
             }
             else {
-                parentValue[lastElement] = value;
+                const lastPatternElement = propInfo.patternElements[propInfo.patternElements.length - 1];
+                const lastElement = propInfo.elements[propInfo.elements.length - 1];
+                const parentValue = this._getValue(target, propInfo.patternPaths, propInfo.patternElements, indexes, propInfo.paths.length - 2, propInfo.wildcardCount - (lastPatternElement === "*" ? 1 : 0) - 1, receiver);
+                if (lastPatternElement === "*") {
+                    parentValue[indexes[indexes.length - 1] ?? utils.raise("wildcard is undefined")] = value;
+                }
+                else {
+                    parentValue[lastElement] = value;
+                }
             }
         });
         return true;
